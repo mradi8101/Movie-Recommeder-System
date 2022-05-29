8d9d6b2a56c4466225df32ee8538972197dad7a3
mradi8101 <95172972+mradi8101@users.noreply.github.com>
2022-05-29 12:57:12 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -171,4 +171,4 @@ def recommend():
         vote_count=vote_count,release_date=release_date,movie_rel_date=movie_rel_date,curr_date=curr_date,runtime=runtime,status=status,genres=genres,movie_cards=movie_cards,reviews=movie_reviews,casts=casts,cast_details=cast_details)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080)
+    app.run(debug=True)
